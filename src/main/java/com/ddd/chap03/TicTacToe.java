b47d7fd4f17e3511efb40747ccc525550840c05b
ChengLu <2854859592@qq.com>
2022-11-03 00:54:26 +0800

    add isWin&&isDraw

--- src/main/java/com/ddd/chap03/TicTacToe.java
+++ src/main/java/com/ddd/chap03/TicTacToe.java
@@ -7,17 +7,25 @@ public class TicTacToe {
             {'\0', '\0', '\0'},
             {'\0', '\0', '\0'}
     };
-    private Character nextPlayer = '\0';
+    private Character lastPlayer = '\0';
+    final int SIZE = 3;
 
-    public void play(int x, int y) {
+    public String play(int x, int y) {
         checkAxis(x);
         checkAxis(y);
-        setBox(x, y);
-        nextPlayer = nextPlayer();
+        lastPlayer = nextPlayer();
+        setBox(x, y, lastPlayer);
+        if (isWin(x, y)) {
+            return String.format("%c is winner", lastPlayer);
+        }
+        if (isDraw()) {
+            return "the result is draw";
+        }
+        return "no winner";
     }
 
     public char nextPlayer() {
-        if (nextPlayer == 'X') {
+        if (lastPlayer == 'X') {
             return 'O';
         }
         return 'X';
@@ -29,10 +37,37 @@ public class TicTacToe {
         }
     }
 
-    private void setBox(int x, int y) {
+    private void setBox(int x, int y, Character lastPlayer) {
         if(board[x-1][y-1] != '\0') {
             throw new RuntimeException("position is occupied!");
         }
-        board[x-1][y-1] = 'X';
+        board[x-1][y-1] = lastPlayer;
+    }
+
+    boolean isWin(int x, int y) {
+        int playerTotal = lastPlayer * 3;
+        int vertical, horizontal, dialog1, dialog2;
+        vertical = horizontal = dialog1 = dialog2 = '\0';
+        for (int i=0; i<SIZE; i++) {
+            vertical += board[i][y-1];
+            horizontal += board[x-1][i];
+            dialog1 += board[i][i];
+            dialog2 += board[i][SIZE-1-i];
+        }
+        if (playerTotal == vertical || playerTotal == horizontal || playerTotal == dialog1 || playerTotal == dialog2) {
+            return true;
+        }
+        return false;
+    }
+
+    boolean isDraw() {
+        for (int i=0; i<SIZE; i++) {
+            for (int j=0; j<SIZE; j++) {
+                if (board[i][j] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- src/test/java/com/ddd/chap03/TicTacToeTest.java
+++ src/test/java/com/ddd/chap03/TicTacToeTest.java
@@ -40,4 +40,65 @@ class TicTacToeTest {
         ticTacToe.play(1,2);
         Assertions.assertEquals('O', ticTacToe.nextPlayer());
     }
+
+    @Test
+    public void whenPlayThenNoWinner() {
+        String actual = ticTacToe.play(1, 2);
+        Assertions.assertEquals("no winner", actual);
+    }
+
+    @Test
+    public void whenPlayWholeHorizontalLineThenWinner() {
+        ticTacToe.play(1, 1); // X
+        ticTacToe.play(2, 1); // O
+        ticTacToe.play(1, 2); // X
+        ticTacToe.play(2, 2); // O
+        String actual = ticTacToe.play(1, 3);// X
+        assertEquals("X is winner", actual);
+    }
+
+
+    @Test
+    public void whenPlayWholeVerticalLineThenWinner() {
+        ticTacToe.play(1, 1); // X
+        ticTacToe.play(1, 2); // O
+        ticTacToe.play(2, 1); // X
+        ticTacToe.play(1, 3); // O
+        String actual = ticTacToe.play(3, 1);// X
+        Assertions.assertEquals("X is winner", actual);
+    }
+
+    @Test
+    public void whenPlayTop2BottomDialogLineThenWinner() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(2, 1);
+        String actual = ticTacToe.play(3, 3);
+        Assertions.assertEquals("X is winner", actual);
+    }
+
+    @Test
+    public void whenPlayBottom2TopDialogLineThenWinner() {
+        ticTacToe.play(1, 3);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(2, 1);
+        String play = ticTacToe.play(3, 1);
+        Assertions.assertEquals("X is winner", play);
+    }
+
+    @Test
+    public void whenAllBoxesFilledThenDraw() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(1, 2);
+        ticTacToe.play(1, 3);
+        ticTacToe.play(2, 1);
+        ticTacToe.play(2, 3);
+        ticTacToe.play(2, 2);
+        ticTacToe.play(3, 1);
+        ticTacToe.play(3, 3);
+        String play = ticTacToe.play(3, 2);
+        Assertions.assertEquals("the result is draw", play);
+    }
 }
