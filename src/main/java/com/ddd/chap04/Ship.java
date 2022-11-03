9ed3d22406b1b14b0d62faa8333be3372fd9f2eb
ChengLu <2854859592@qq.com>
2022-11-03 23:17:39 +0800

    add 行覆盖率；ship

---
+++ src/main/java/com/ddd/chap04/Ship.java
@@ -0,0 +1,5 @@
+package com.ddd.chap04;
+
+public class Ship {
+
+}
---
+++ src/test/java/com/ddd/chap04/ShipSpec.java
@@ -0,0 +1,10 @@
+package com.ddd.chap04;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+@Test
+public class ShipSpec {
+
+}
