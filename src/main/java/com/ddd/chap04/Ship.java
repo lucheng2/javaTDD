98e6b28d536b001b5517d59d15acc5649f4d030c
ChengLu <2854859592@qq.com>
2022-11-04 00:22:41 +0800

    add forward&backward

--- src/main/java/com/ddd/chap04/Ship.java
+++ src/main/java/com/ddd/chap04/Ship.java
@@ -2,4 +2,21 @@ package com.ddd.chap04;
 
 public class Ship {
 
+    private final Location location;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public boolean moveForward() {
+        return location.forward();
+    }
+
+    public boolean moveBackword() {
+        return location.backward();
+    }
 }
--- src/test/java/com/ddd/chap04/ShipSpec.java
+++ src/test/java/com/ddd/chap04/ShipSpec.java
@@ -1,10 +1,38 @@
 package com.ddd.chap04;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import static org.testng.Assert.*;
 
 @Test
 public class ShipSpec {
+
+    private Ship ship;
+    private Location location;
+
+    @BeforeTest
+    public void beforeTest() {
+        location = new Location(new Point(1,2), Direction.NORTH);
+        ship = new Ship(location);
+    }
+
+    public void whenInstantiatedThenPositionIsSet() {
+        Assert.assertEquals(location, ship.getLocation());
+    }
+
+    public void whenMoveForwardThenForward() {
+        Location expected = location.copy();
+        expected.forward();
+        ship.moveForward();
+        Assert.assertEquals(expected, ship.getLocation());
+    }
+
+    public void whenMoveBackwardThenBackward() {
+        Location expected = location.copy();
+        expected.backward();
+        ship.moveBackword();
+        Assert.assertEquals(expected, ship.getLocation());
+    }
 
 }
